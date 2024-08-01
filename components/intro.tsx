941c67b411224098b515b35688892a17b7ce3fa0
Chhavikant Mahobia <ckcodes907@gmail.com>
2024-08-02 00:01:27 +0530

    poster url updated

--- components/intro.tsx
+++ components/intro.tsx
@@ -73,7 +73,7 @@ export default function Intro() {
       >
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="https://drive.google.com/uc?export=download&id=1tUGJnAYLCrhQx8zXZZorchQuN4vQ8fmP"
+          href="https://drive.google.com/uc?export=download&id=1szGnRFXG9ich8qBzETcbvExQ7-XPWl0d"
           download
         >
           Download Poster
