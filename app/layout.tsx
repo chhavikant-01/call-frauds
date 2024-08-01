30a0e0cdaf537419d8b916d5f71ee538e26b121f
Chhavikant Mahobia <ckcodes907@gmail.com>
2024-08-01 23:35:29 +0530

    final changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ricardo | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
+  title: "Phone call fraud",
+  description: "Challenges, Trends, and Countermeasures",
 };
 
 export default function RootLayout({
